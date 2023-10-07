a19acd005f6f1b25263111a7284a7fa1769fc666
Ayan Dutt Pandey <101915400+fakeayyann@users.noreply.github.com>
2023-10-07 15:44:05 +0530

    Update Cricket Score.py

--- Cricket Score.py
+++ Cricket Score.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[38]:
-
-
 player_score =  {}
-with open("scores.csv","r") as rd:
+with open("you_file","r") as rd: #your file path/name
     for line in rd:
         player,score = line.split(',')
         score = int(score)
@@ -13,32 +7,13 @@ with open("scores.csv","r") as rd:
             player_score[player].append(score)
         else:
             player_score[player] = [score]
-print(player_score)
-
-
-# In[ ]:
-
-
-
-
-
-# In[40]:
-
-
-for i,j in player_score.items():
+print(player_score)for i,j in player_score.items():
     minj = min(j)
     maxj = max(j)
     avgj = sum(j)/len(j)
     print(f"the minimum for {i} is {minj} , max is {maxj} and avg is {avgj}")
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
